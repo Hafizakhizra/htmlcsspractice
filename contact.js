1c9d07c948c2b7bff076d1b392610551efdc98ae
Hafizakhizra <91067375+Hafizakhizra@users.noreply.github.com>
2023-06-18 23:04:13 +0500

    htmlcssprojectsetup

---
+++ contact.js
@@ -0,0 +1,43 @@
+//document.getElementById("demo").innerHTML="Hey.";
+//alert("Hello World!")
+
+//const cars = [saab, BMW, safari];
+//document.getElementById("demo").innerHTML=cars;
+//console.log(cars)
+// document.write("Hello WORLD");
+// console.log("Hello", 7+6, "World");
+// console.error("Error found");
+
+// Variables
+var num1= 40;
+var num2= 30;
+//console.log(num1 + num2);
+
+// Data types
+var num3 = 4.5;
+var str = "This is string.";
+var str2 = 'Tis is also a heading.';
+// console.log(str2);
+//  Objests
+var marks = {
+    ali: 66,
+    farooq: 80,
+    faheem: 99.5
+}
+//console.log(marks.ali);
+// Boolean
+var a = true;
+var b = false;
+//console.error(a, b);
+
+function sum(a, b){
+    return a+b;
+}
+c = sum(3, 6);
+//console.log(c);
+
+
+
+
+
+
